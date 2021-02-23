eed09d0e518afa260c3b6bb5670781b6098bd8d1
Vignesh waran <vw170996@gmail.com>
2021-02-23 14:14:04 +0530

    final

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,15 +5,23 @@ const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 let makeItThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
+function choice(num) {
+  let id = Math.floor(Math.random() * num.length);
+  return num[id];
+}
+
 class Pokecard extends Component {
   render() {
+
     let id = makeItThree(this.props.id);
     let imgSrc = `${POKE_API}${id}.png`;
     
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
+        <div className="Pokecard-img">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
